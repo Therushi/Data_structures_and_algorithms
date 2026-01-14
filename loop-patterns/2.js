dfa646df257305e6101c498f657b2c76ab7c771d
rushi <rushidhanawade123@gmail.com>
2026-01-14 15:10:29 +0530

    number patterns

--- loop-patterns/2.js
+++ loop-patterns/2.js
@@ -10,5 +10,32 @@ for (let i = 0; i < n; i++) {
   for (let j = 0; j < i + 1; j++) {
     rows += "*";
   }
+  //   console.log(rows);
+}
+
+//1
+//12
+//123
+//1234
+
+for (let i = 0; i < n; i++) {
+  let rows = "";
+  for (let j = 0; j < i + 1; j++) {
+    rows += j + 1;
+  }
+  //   console.log(rows);
+}
+
+//1
+//22
+//333
+//4444
+//55555
+
+for (let i = 0; i < n; i++) {
+  let rows = "";
+  for (let j = 0; j < i + 1; j++) {
+    rows += i + 1;
+  }
   console.log(rows);
 }
